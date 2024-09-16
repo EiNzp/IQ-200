b08a259a9e75ff390d4e2564a361d4a82166aba5
EiNzp <116449840+EiNzp@users.noreply.github.com>
2024-09-16 22:11:45 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -74,8 +74,22 @@ if (subLists.length > 0) {
 // Получаем элементы, с которыми будем работать
 const menu = document.querySelector('.header'); // Всё меню
 const menuItem = document.querySelector('.info-body-header__info'); // Определённый элемент меню
-const block1 = document.querySelector('.main-block-main-page__title'); // Первый блок
-const block2 = document.querySelector('.educational-center__header-title'); // Второй блок
+// const block1 = document.querySelector('.main-block-main-page__title'); // Первый блок
+// const block2 = document.querySelector('.educational-center__header-title'); // Второй блок
+
+let block1
+if (document.querySelector('.main-block-main-page__title')) {
+  block1 = document.querySelector('.main-block-main-page__title');
+} else if (document.querySelector('.main-block-preparation-for-school__title')) {
+  block1 = document.querySelector('.main-block-preparation-for-school__title');
+}
+
+let block2
+if (document.querySelector('.educational-center__header-title')) {
+  block2 = document.querySelector('.educational-center__header-title');
+} else if (document.querySelector('.production-and-delivery__header')) {
+  block2 = document.querySelector('.production-and-delivery__header')
+}
 
 let menuHidden = false; // Флаг, показывающий, что меню задвинуто
 let menuItemHidden = false; // Флаг, показывающий, что элемент меню скрыт
